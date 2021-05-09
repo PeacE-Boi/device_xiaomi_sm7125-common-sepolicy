18c6a47a0f085f5c8554044f2f5e06cad6ff2bc6
magicxavi <rolfpeterkesche@gmail.com>
2021-05-09 05:10:42 +0000

    sm7125-common: sepolicy: guard sepolicy build with target device filter

---
+++ sepolicy.mk
@@ -0,0 +1,10 @@
+# Device specific SELinux policy variable definitions
+SEPOLICY_PATH:= device/xiaomi/sm7125-common-sepolicy
+
+ifneq ($(filter excalibur,$(TARGET_DEVICE)),)
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(SEPOLICY_PATH)/private
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(SEPOLICY_PATH)/public
+BOARD_VENDOR_SEPOLICY_DIRS += $(SEPOLICY_PATH)/vendor
+
+endif
