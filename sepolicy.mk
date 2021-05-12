02be311b7a98cb59593b6ef61cad93560a3da6ed
Demon000 <demonsingur@gmail.com>
2021-05-12 13:13:15 +0000

    sm7125-common: sepolicy: guard all mi-atoll devices

--- sepolicy.mk
+++ sepolicy.mk
@@ -1,7 +1,7 @@
 # Device specific SELinux policy variable definitions
 SEPOLICY_PATH:= device/xiaomi/sm7125-common-sepolicy
 
-ifneq ($(filter excalibur,$(TARGET_DEVICE)),)
+ifneq ($(filter excalibur miatoll curtana gram joyeuse,$(TARGET_DEVICE)),)
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(SEPOLICY_PATH)/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(SEPOLICY_PATH)/public
